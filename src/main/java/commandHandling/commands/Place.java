7a7b831edd8deefc283893931daa446e9ab4e7c7
h4ndr <i9$..#$m1c,[2pmEBSZ{vla7tfwpcWX69^t8)8]N{;d)ZvZKn?%]GF]ozTJd@>
2021-03-23 22:45:17 +0100

    added aliases and clean up

--- src/main/java/commandHandling/commands/Place.java
+++ src/main/java/commandHandling/commands/Place.java
@@ -1,50 +1,49 @@
 package commandHandling.commands;
 
 import commandHandling.commands.place.*;
-import commandHandling.CommandContext;
-import commandHandling.CommandInterface;
+import commandHandling.*;
 import org.slf4j.Logger;
-import services.BotExceptions;
+import services.*;
 
 public class Place implements CommandInterface {
+    private static draw drawInstance = null;
+
     public Place(Logger LOGGER) {
         LOGGER.info("Loaded Command Place");
     }
 
     @Override
     public void handle(CommandContext ctx) {
-        draw drawInstance = null;
 
         switch (ctx.getArguments().get(0)) {
-            case "encode":
+            case "encode": case "e":
                 if (ctx.getArguments().size() < 5) BotExceptions.invalidArgumentsException(ctx);
-                 else new encode(ctx);
+                else new encode(ctx);
                 break;
-            case "preview":
+            case "preview": case "p":
                 new preview(ctx);
                 break;
-            case "draw":
-                if (drawInstance == null || !drawInstance.drawing)
-                    drawInstance = new draw(ctx);
-                else
-                    ctx.getMessage().reply("Already Drawing \n Progress: " +
+            case "draw": case "d":
+                if (drawInstance == null || !drawInstance.drawing) drawInstance = new draw(ctx);
+                else ctx.getMessage().reply("Already Drawing \n Progress: " +
                             String.format("%,.2f", drawInstance.progress)).queue();
                 break;
-            case "queue":
+            case "queue": case "q":
                 new queue(ctx);
                 break;
-            case "stop":
-                if (drawInstance != null)
-                    drawInstance.draw = false;
-                else
-                    ctx.getChannel().sendMessage("Currently not drawing").queue();
+            case "stop": case "s":
+                if (drawInstance != null) drawInstance.draw = false;
+                else ctx.getChannel().sendMessage("Currently not drawing").queue();
                 break;
-            case "stopQ":
-                drawInstance.stopQ = true;
+            case "stopQ": case "stopq": case "sq":
+                if (drawInstance != null) drawInstance.stopQ = true;
                 break;
             default:
                 BotExceptions.commandNotFoundException(ctx, ctx.getArguments().get(0));
+                CommandReaction.fail(ctx);
+                return;
         }
+        CommandReaction.success(ctx);
     }
 
     @Override
