693612e572c4bce42cc2ebf1c33e4388e0d59219
h4ndr <i9$..#$m1c,[2pmEBSZ{vla7tfwpcWX69^t8)8]N{;d)ZvZKn?%]GF]ozTJd@>
2021-05-09 22:14:20 +0200

    added gif previews

--- src/main/java/commandHandling/commands/place/preview.java
+++ src/main/java/commandHandling/commands/place/preview.java
@@ -2,17 +2,17 @@ package commandHandling.commands.place;
 
 import commandHandling.CommandContext;
 import services.BotExceptions;
+import services.GifSequenceWriter;
 import services.PlaceWebSocket;
 
-import javax.imageio.ImageIO;
-import java.awt.Color;
+import javax.imageio.stream.FileImageOutputStream;
+import javax.imageio.stream.ImageOutputStream;
+import java.awt.*;
 import java.awt.image.BufferedImage;
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
+import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
+import java.util.ArrayList;
 import java.util.Scanner;
-import java.util.concurrent.TimeUnit;
 
 public class preview implements Runnable{
     private final CommandContext ctx;
@@ -23,7 +23,8 @@ public class preview implements Runnable{
 
     @Override
     public void run() {
-        BufferedImage img = PlaceWebSocket.getImage();
+        BufferedImage place = PlaceWebSocket.getImage(false);
+        BufferedImage img = new BufferedImage(1000, 1000, BufferedImage.TYPE_INT_ARGB);
         Scanner scanner;
 
         try {
@@ -38,33 +39,34 @@ public class preview implements Runnable{
             }
         }
 
-        for (int i = 0; i < img.getHeight(); i++) {
-            for (int j = 0; j < img.getWidth(); j++) {
-                Color color = new Color(img.getRGB(i, j));
-                int rgb = (int)(color.getRed() * 0.299);
-                rgb += (color.getGreen() * 0.587) + (color.getBlue() * 0.114);
-                img.setRGB(i, j, new Color(rgb, rgb, rgb).getRGB());
-            }
-        }
-
-        while (scanner.hasNextLine()) {
-            Scanner s = new Scanner(scanner.nextLine().substring(16));
-            img.setRGB(s.nextInt(), s.nextInt(), Color.decode(s.next()).getRGB());
-            s.close();
-        }
-
-        ctx.getChannel().sendMessage("Preview").addFile(convert(img), "preview.png").queue(
-                msg -> msg.delete().queueAfter(64, TimeUnit.SECONDS)
-        );
-    }
-
-    private InputStream convert (BufferedImage img) {
-        ByteArrayOutputStream os = new ByteArrayOutputStream();
         try {
-            ImageIO.write(img, "png", os);
+            ImageOutputStream output = new FileImageOutputStream(new File("tempFiles/place/preview.gif"));
+            GifSequenceWriter writer = new GifSequenceWriter(output, BufferedImage.TYPE_INT_ARGB, 50, true);
+            ArrayList<String[]> pixels = new ArrayList<>();
+
+            while (scanner.hasNextLine()) {
+                pixels.add(scanner.nextLine().split(" "));
+            }
+
+            int pixelsPerFrame = (int)(pixels.size() * 0.012);
+            writer.writeToSequence(place);
+
+            for (int i = 0; i < pixels.size(); i++) {
+                img.setRGB(Integer.parseInt(pixels.get(i)[2]), Integer.parseInt(pixels.get(i)[3]), Color.decode(pixels.get(i)[4]).getRGB());
+                if (i % pixelsPerFrame == 0) {
+                    writer.writeToSequence(img);
+                }
+            }
+
+            scanner.close();
+            writer.close();
+            output.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
-        return new ByteArrayInputStream(os.toByteArray());
+
+        File gif = new File("tempFiles/place/preview.gif");
+        ctx.getChannel().sendFile(gif).complete();
+        gif.delete();
     }
 }
