54da47d0a8a676058238f716f2168a5b517a0b22
h4ndr <i9$..#$m1c,[2pmEBSZ{vla7tfwpcWX69^t8)8]N{;d)ZvZKn?%]GF]ozTJd@>
2021-06-05 21:02:51 +0200

    made discord reload the user cache

--- src/main/java/commandHandling/commands/SemesterSpokesPeople.java
+++ src/main/java/commandHandling/commands/SemesterSpokesPeople.java
@@ -3,13 +3,20 @@ package commandHandling.commands;
 import commandHandling.CommandContext;
 import commandHandling.CommandInterface;
 import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.entities.Message;
+import org.slf4j.Logger;
 import services.database.dbHandlerSpokesPeople;
 
 import java.awt.*;
+import java.io.File;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 public class SemesterSpokesPeople implements CommandInterface {
+    public SemesterSpokesPeople(Logger LOGGER) {
+        LOGGER.info("Loaded Command SemesterSpokesPeople");
+    }
+
     @Override
     public void handle(CommandContext ctx) {
         EmbedBuilder embed = new EmbedBuilder();
@@ -18,11 +25,17 @@ public class SemesterSpokesPeople implements CommandInterface {
 
         embed.setTitle("Semester Spokes People");
         embed.setColor(new Color(0xb074ad));
+        embed.setThumbnail("attachment://vis.png");
         embed.addField("__First Year__", firstYear, true);
         embed.addBlankField(true);
         embed.addField("__Second Year__", secondYear, true);
 
-        ctx.getChannel().sendMessage(embed.build()).queue(msg -> msg.delete().queueAfter(32, TimeUnit.SECONDS));
+        Message discordCacheRefresh = ctx.getChannel().sendMessage("beep boop").complete();
+        discordCacheRefresh.editMessage(firstYear + "\n" + secondYear).complete();
+        discordCacheRefresh.delete().queue();
+
+        ctx.getChannel().sendMessage(embed.build()).addFile(new File("resources/vis.png"))
+                .queue(msg -> msg.delete().queueAfter(32, TimeUnit.SECONDS));
     }
 
     @Override
