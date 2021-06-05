1890922a3466c95d988871a136532e481acf02f5
h4ndr <i9$..#$m1c,[2pmEBSZ{vla7tfwpcWX69^t8)8]N{;d)ZvZKn?%]GF]ozTJd@>
2021-06-05 20:52:40 +0200

    added command kill

---
+++ src/main/java/commandHandling/commands/Kill.java
@@ -0,0 +1,39 @@
+package commandHandling.commands;
+
+import commandHandling.CommandContext;
+import commandHandling.CommandInterface;
+import net.dv8tion.jda.api.EmbedBuilder;
+import org.slf4j.Logger;
+import resources.CONFIG;
+
+import java.awt.*;
+import java.io.File;
+
+public class Kill implements CommandInterface {
+    public Kill(Logger LOGGER) {
+        LOGGER.info("Loaded Command Kill");
+    }
+
+    @Override
+    public void handle(CommandContext ctx) {
+        if (ctx.getAuthor().getId().equals(CONFIG.OwnerID.get())) {
+            services.Logger.botStatus(ctx.getJDA(), ctx.getSelfUser().getName() + " is committing sudoku", "");
+            ctx.getChannel().sendMessage("Shutting down").addFile(new File("resources/shutdown.gif")).queue();
+            ctx.getJDA().shutdownNow();
+        }
+    }
+
+    @Override
+    public String getName() {
+        return "kill";
+    }
+
+    @Override
+    public EmbedBuilder getHelp() {
+        EmbedBuilder embed = new EmbedBuilder();
+        embed.setTitle("Help - Kill");
+        embed.setColor(new Color(0xb074ad));
+        embed.setDescription("Shuts down the JDA instance and cancels all pending rest actions");
+        return embed;
+    }
+}
