f0b7ddd2a1b8ffd5f73ea3a5d1b796680ce03b31
h4ndr <i9$..#$m1c,[2pmEBSZ{vla7tfwpcWX69^t8)8]N{;d)ZvZKn?%]GF]ozTJd@>
2021-06-05 20:54:08 +0200

    Added shutdown command

---
+++ src/main/java/commandHandling/commands/Shutdown.java
@@ -0,0 +1,39 @@
+package commandHandling.commands;
+
+import commandHandling.CommandContext;
+import commandHandling.CommandInterface;
+import net.dv8tion.jda.api.EmbedBuilder;
+import org.slf4j.Logger;
+import resources.CONFIG;
+
+import java.awt.*;
+import java.io.File;
+
+public class Shutdown implements CommandInterface {
+    public Shutdown(Logger LOGGER) {
+        LOGGER.info("Loaded Command Shutdown");
+    }
+
+    @Override
+    public void handle(CommandContext ctx) {
+        if (ctx.getAuthor().getId().equals(CONFIG.OwnerID.get())) {
+            services.Logger.botStatus(ctx.getJDA(), ctx.getSelfUser().getName() + " is shutting down", "");
+            ctx.getChannel().sendMessage("Shutting down").addFile(new File("resources/shutdown.gif")).queue();
+            ctx.getJDA().shutdown();
+        }
+    }
+
+    @Override
+    public String getName() {
+        return "shutdown";
+    }
+
+    @Override
+    public EmbedBuilder getHelp() {
+        EmbedBuilder embed = new EmbedBuilder();
+        embed.setTitle("Help - Shutdown");
+        embed.setColor(new Color(0xb074ad));
+        embed.setDescription("Shuts down the JDA instance but lets pending rest actions execute");
+        return embed;
+    }
+}
