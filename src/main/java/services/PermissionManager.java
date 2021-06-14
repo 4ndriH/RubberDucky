0545fd99c481ed6b88a3fb68b438f631ebcbaec9
h4ndr <i9$..#$m1c,[2pmEBSZ{vla7tfwpcWX69^t8)8]N{;d)ZvZKn?%]GF]ozTJd@>
2021-06-14 22:11:01 +0200

    implemented working PermissionManager

--- src/main/java/services/PermissionManager.java
+++ src/main/java/services/PermissionManager.java
@@ -1,47 +1,28 @@
 package services;
 
-import resources.CONFIG;
-import services.database.dbHandlerPermissions;
 import commandHandling.CommandContext;
-import org.slf4j.LoggerFactory;
-import org.slf4j.Logger;
-import java.util.*;
+import resources.CONFIG;
 
 public class PermissionManager {
-    private static final Logger LOGGER = LoggerFactory.getLogger(PermissionManager.class);
+    public static boolean permissionCheck(CommandContext ctx, String invoke, CommandManager cm) {
+        return authenticateOwner(ctx) || serverCheck(ctx) && !blackListCheck(ctx) && channelCheck(ctx, invoke)
+                && !cm.getCommand(invoke).isOwnerOnly();
 
-    public static boolean permissionCheck(CommandContext ctx, String invoke) {
-        return authenticateOwner(ctx) || serverCheck(ctx) && roleCheck(ctx, invoke) && channelCheck(ctx, invoke) &&
-                !blackListCheck(ctx);
     }
 
     public static boolean authenticateOwner(CommandContext ctx) {
         return ctx.getAuthor().getId().equals(CONFIG.OwnerID.get());
     }
 
-    public static boolean serverCheck (CommandContext ctx) {
-        return dbHandlerPermissions.server(ctx.getGuild().getId());
-    }
-
-    public static boolean roleCheck (CommandContext ctx, String invoke) {
-        ArrayList<String> permittedRoles = dbHandlerPermissions.roles(invoke);
-
-        for (String rl : permittedRoles) {
-            if (ctx.getMember().getRoles().contains(ctx.getGuild().getRoleById(rl))) {
-                return true;
-            }
-        }
-
-        return permittedRoles.size() == 0;
+    public static boolean blackListCheck (CommandContext ctx) {
+        return CONFIG.getBlackList().contains(ctx.getAuthor().getId());
     }
 
     public static boolean channelCheck (CommandContext ctx, String invoke) {
-        ArrayList<String> permittedChannels = dbHandlerPermissions.channels(invoke);
-
-        return permittedChannels.size() == 0 || permittedChannels.contains(ctx.getChannel().getId());
+        return CONFIG.commandChannelCheck(invoke, ctx.getChannel().getId());
     }
 
-    public static boolean blackListCheck (CommandContext ctx) {
-        return dbHandlerPermissions.blackList(ctx.getAuthor().getId());
+    public static boolean serverCheck (CommandContext ctx) {
+        return CONFIG.getServers().contains(ctx.getGuild().getId());
     }
 }
