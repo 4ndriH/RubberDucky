7c92a48f361e2e13ca6d6b54f3b4152395416672
4ndriH <handri@bluewin.ch>
2021-07-17 23:54:25 +0200

    added cool down feature

---
+++ src/main/java/services/CoolDownManager.java
@@ -0,0 +1,67 @@
+package services;
+
+import commandHandling.CommandContext;
+import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.entities.MessageEmbed;
+
+import java.awt.*;
+import java.util.HashMap;
+
+public class CoolDownManager {
+    private static HashMap<String, HashMap<String, Long>> coolDown;
+
+    public CoolDownManager() {
+        coolDown = new HashMap<>();
+    }
+
+    public static boolean coolDownCheck(CommandContext ctx, String command) {
+        String user = ctx.getAuthor().getId();
+
+        if (coolDown.containsKey(user) && coolDown.get(user).containsKey(command)) {
+            long coolDownMS = coolDown.get(user).get(command);
+            if (coolDownMS > System.currentTimeMillis()) {
+                ctx.getMessage().replyEmbeds(buildEmbed(coolDownMS, command)).queue(
+                        msg -> Miscellaneous.deleteMsg(msg, 32)
+                );
+                return true;
+            } else {
+                coolDown.get(user).remove(command);
+            }
+        }
+        return false;
+    }
+
+    public static void addCoolDown(String command, String user, int seconds) {
+        long coolDownTime = System.currentTimeMillis() + seconds * 1000L;
+        if (!coolDown.containsKey(user)){
+            coolDown.put(user, new HashMap<>());
+        }
+        if (!coolDown.get(user).containsKey(command)) {
+            coolDown.get(user).put(command, coolDownTime);
+        } else {
+            coolDown.get(user).replace(command, coolDownTime);
+        }
+    }
+
+    private static MessageEmbed buildEmbed(Long ms, String command) {
+        EmbedBuilder embed = new EmbedBuilder();
+        embed.setTitle("CoolDown - " + command);
+        embed.setColor(new Color(0xb074ad));
+        embed.setDescription(timeRemaining(ms - System.currentTimeMillis()));
+        return embed.build();
+    }
+
+    private static String timeRemaining(Long ms) {
+        StringBuilder time = new StringBuilder();
+
+        long hours = ms / 3_600_000;
+        long minutes = ms / 60_000 - hours * 60;
+        long seconds = ms / 1000 - minutes * 60 - hours * 3600;
+
+        time.append(hours < 10 ? "0" : "").append(hours)
+                .append(minutes < 10 ? ":0" : ":").append(minutes)
+                .append(seconds < 10 ? ":0" : ":").append(seconds);
+
+        return time.toString();
+    }
+}
--- src/main/java/services/PermissionManager.java
+++ src/main/java/services/PermissionManager.java
@@ -5,24 +5,27 @@ import resources.CONFIG;
 
 public class PermissionManager {
     public static boolean permissionCheck(CommandContext ctx, String invoke, CommandManager cm) {
-        return authenticateOwner(ctx) || serverCheck(ctx) && !blackListCheck(ctx) && channelCheck(ctx, invoke)
-                && !cm.getCommand(invoke).isOwnerOnly();
-
+        return authenticateOwner(ctx) || !cm.getCommand(invoke).isOwnerOnly() && serverCheck(ctx)
+                && !blackListCheck(ctx) && channelCheck(ctx, invoke) && !coolDownCheck(ctx, invoke);
     }
 
     public static boolean authenticateOwner(CommandContext ctx) {
         return ctx.getAuthor().getId().equals(CONFIG.OwnerID.get());
     }
 
-    public static boolean blackListCheck (CommandContext ctx) {
+    public static boolean blackListCheck(CommandContext ctx) {
         return CONFIG.getBlackList().contains(ctx.getAuthor().getId());
     }
 
-    public static boolean channelCheck (CommandContext ctx, String invoke) {
-        return CONFIG.commandChannelCheck(invoke, ctx.getChannel().getId());
+    public static boolean channelCheck(CommandContext ctx, String invoke) {
+        return CONFIG.channelCheck(invoke, ctx.getChannel().getId());
     }
 
-    public static boolean serverCheck (CommandContext ctx) {
+    public static boolean serverCheck(CommandContext ctx) {
         return CONFIG.getServers().contains(ctx.getGuild().getId());
+    }
+
+    public static boolean coolDownCheck(CommandContext ctx, String command) {
+        return CoolDownManager.coolDownCheck(ctx, command);
     }
 }
