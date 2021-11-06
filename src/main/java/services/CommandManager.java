75fa5a70406c2cba1dc5c627bde5954e22dc964a
4ndriH <handri@bluewin.ch>
2021-11-06 22:04:57 +0100

    Added simple about page with github link

--- src/main/java/services/CommandManager.java
+++ src/main/java/services/CommandManager.java
@@ -38,6 +38,7 @@ public class CommandManager {
         addCommand(new ProfilePicture(LOGGER));
         addCommand(new Status(LOGGER));
         addCommand(new Say(LOGGER));
+        addCommand(new About(LOGGER));
     }
 
     private void addCommand(CommandInterface cmd) {
