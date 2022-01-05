7d559de2474e768a6a2fe6be08fe618b5afba42e
4ndriH <handri@bluewin.ch>
2022-01-05 21:21:57 +0100

    course now shows you a list of courses with reviews

--- src/main/java/commandHandling/commands/publicCommands/CourseReview/Course.java
+++ src/main/java/commandHandling/commands/publicCommands/CourseReview/Course.java
@@ -9,6 +9,9 @@ import resources.CONFIG;
 import services.database.DatabaseHandler;
 import services.logging.EmbedHelper;
 
+import java.util.ArrayList;
+import java.util.stream.Collectors;
+
 public class Course implements CommandInterface {
     private static final Logger LOGGER = LoggerFactory.getLogger(Course.class);
 
@@ -18,16 +21,20 @@ public class Course implements CommandInterface {
 
     @Override
     public void handle(CommandContext ctx) {
+        ArrayList<String> reviews;
         EmbedBuilder embed = EmbedHelper.embedBuilder();
-        if (ctx.getArguments().size() == 0 || DatabaseHandler.containsCourseNumber(ctx.getArguments().get(0)) <= 0) {
-            embed.setTitle("Looks like there are no reviews for course: "
-                    + (ctx.getArguments().size() > 0 ? ctx.getArguments().get(0) : ""));
+        if (ctx.getArguments().size() == 0 || DatabaseHandler.containsCourseNumber(ctx.getArguments().get(0)) <= 0 || (reviews = DatabaseHandler.getCourseReview(ctx.getArguments().get(0))).size() == 0) {
+            ArrayList<String> courses = DatabaseHandler.getAllCourses();
+
+            embed.setTitle("Looks like there are no (verified) reviews for course: " + (ctx.getArguments().size() > 0 ? ctx.getArguments().get(0) : "-"));
+            embed.addField("Courses with reviews", courses.stream().skip(1).map(Object::toString).collect(Collectors.joining("\n")), false);
         } else {
             String title = DatabaseHandler.getCourse(ctx.getArguments().get(0));
+            StringBuilder sb = new StringBuilder();
             int titleLength = title.length();
             embed.setTitle(title);
-            StringBuilder sb = new StringBuilder();
-            for (String s : DatabaseHandler.getCourseReview(ctx.getArguments().get(0))) {
+
+            for (String s : reviews) {
                 if (sb.length() + titleLength + s.length() < 6000) {
                     sb.append("```").append(s).append("```");
                 } else {
@@ -48,7 +55,7 @@ public class Course implements CommandInterface {
     @Override
     public EmbedBuilder getHelp() {
         EmbedBuilder embed = new EmbedBuilder();
-        embed.setDescription("Get reviews for a specific course");
+        embed.setDescription("Get reviews for a specific course. If the course has no reviews or no course number is provided you get a list with all available courses.");
         embed.addField("__Format__", "```" + CONFIG.Prefix.get() + "course <course number>```", false);
         return embed;
     }
