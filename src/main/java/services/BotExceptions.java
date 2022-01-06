c132579995abc2e379f6c2b6c5cead1f62bc25cf
4ndriH <handri@bluewin.ch>
2022-01-06 21:17:02 +0100

    added an exception for faulty course number and added a arguments boolean

--- src/main/java/services/BotExceptions.java
+++ src/main/java/services/BotExceptions.java
@@ -10,57 +10,61 @@ import java.awt.*;
 public class BotExceptions {
     public static void invalidArgumentsException(CommandContext ctx) {
         sendMessage("invalidArgumentsException", "The given Arguments do not match " +
-                "the required criteria", ctx);
+                "the required criteria", ctx, true);
     }
 
     public static void commandNotFoundException(CommandContext ctx, String command) {
-        sendMessage("commandNotFoundException", "Command \"" + command + "\" not found!", ctx);
+        sendMessage("commandNotFoundException", "Command \"" + command + "\" not found!", ctx, false);
     }
 
     public static void missingAttachmentException(CommandContext ctx) {
-        sendMessage("missingAttachmentException", "No attachment found!", ctx);
+        sendMessage("missingAttachmentException", "No attachment found!", ctx, false);
     }
 
     public static void fileDoesNotExistException(CommandContext ctx) {
-        sendMessage("fileDoesNotExistException", "File does not exist!", ctx);
+        sendMessage("fileDoesNotExistException", "File does not exist!", ctx, true);
     }
 
     public static void missingPermissionException(CommandContext ctx) {
         sendMessage("missingPermissionException",
-                "You do not have the required permissions to run this command!", ctx);
+                "You do not have the required permissions to run this command!", ctx, false);
     }
 
     public static void invalidIdException(CommandContext ctx) {
-        sendMessage("invalidIdException", "This ID does not exist!", ctx);
+        sendMessage("invalidIdException", "This ID does not exist!", ctx, false);
     }
 
     public static void emptyQueueException(CommandContext ctx) {
-        sendMessage("emptyQueueException", "There are no files in the queue!", ctx);
+        sendMessage("emptyQueueException", "There are no files in the queue!", ctx, false);
     }
 
     public static void FileExceedsUploadLimitException(CommandContext ctx) {
         int boost = ctx.getGuild().getBoostCount();
         sendMessage("FileExceedsUploadLimitException","The file exceeds the possible " +
-                (boost <= 1 ? 8 : boost == 2 ? 50 : 100) + "mb!", ctx);
+                (boost <= 1 ? 8 : boost == 2 ? 50 : 100) + "mb!", ctx, false);
     }
 
     public static void unknownPatternException(CommandContext ctx) {
-        sendMessage("unknownPatternException","This is not a valid pattern!", ctx);
+        sendMessage("unknownPatternException","This is not a valid pattern!", ctx, true);
     }
 
     public static void faultyPixelFormatException(CommandContext ctx, String s) {
-        sendMessage("faultyPixelFormatException", "\"" + s + "\" is not a valid setpixel command!", ctx);
+        sendMessage("faultyPixelFormatException", "\"" + s + "\" is not a valid setpixel command!", ctx, false);
+    }
+
+    public static void invalidCourseNumber(CommandContext ctx, String s) {
+        sendMessage("This is not a valid course number", ctx.getArguments().get(0) + s, ctx, false);
     }
 
     // Builds the embed and sends it as a response to a failed sub command
-    private static void sendMessage(String type, String message, CommandContext ctx) {
+    private static void sendMessage(String type, String message, CommandContext ctx, boolean arguments) {
         EmbedBuilder embed = new EmbedBuilder();
 
         embed.setTitle(type);
         embed.setDescription(message);
         embed.setColor(Color.RED);
 
-        if (ctx.getArguments().size() > 1) {
+        if (arguments && ctx.getArguments().size() > 1) {
             StringBuilder args = new StringBuilder();
             for (String s : ctx.getArguments())
                 args.append(s).append(", ");
