6042bb16aa155879b2c0b0b51a4734d73304219e
4ndriH <handri@bluewin.ch>
2022-01-15 23:24:25 +0100

    added another image to cover smth up

--- src/main/java/services/onStartup/DirectoryVerification.java
+++ src/main/java/services/onStartup/DirectoryVerification.java
@@ -42,7 +42,7 @@ public class DirectoryVerification {
                 "ducky3.png", "ducky4.png", "ducky5.png", "ducky6.png", "ducky7.png")));
         files.put("resources/purge/", new ArrayList<>(List.of("busyPurging.png", "purgeCommenced.jpg",
                 "purgeEnded.jpg")));
-        files.put("resources/lmgtfy/", new ArrayList<>(List.of("lmgtfy.png", "lmgtfyResult.png", "cursor0.png", "cursor1.png", "cursor2.png", "cursor3.png", "cursor4.png",
+        files.put("resources/lmgtfy/", new ArrayList<>(List.of("lmgtfy.png", "lmgtfyResult.png", "lmgtfyFix.png", "cursor0.png", "cursor1.png", "cursor2.png", "cursor3.png", "cursor4.png",
                 "cursor5.png", "cursor6.png", "cursor7.png", "cursor8.png", "cursor9.png", "cursor10.png")));
 
         String url;
