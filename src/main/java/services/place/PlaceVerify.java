08e350b7ebfcdfe5c49018274a6f1bf1dc0b2d53
4ndriH <handri@bluewin.ch>
2022-04-16 22:03:00 +0200

    added PlaceVerify functionality

--- src/main/java/services/place/PlaceVerify.java
+++ src/main/java/services/place/PlaceVerify.java
@@ -1,5 +1,7 @@
 package services.place;
 
+import resources.Pixel;
+
 import java.awt.*;
 import java.awt.image.BufferedImage;
 
@@ -7,11 +9,16 @@ public class PlaceVerify {
     public static void verify() {
         BufferedImage place = PlaceWebSocket.getImage(true);
 
+        for (int i = 0; i < PlaceData.drawnPixels; i++) {
+            Pixel pixel = PlaceData.pixels.get(i);
+            Color placeColor = new Color(place.getRGB(pixel.getX(), pixel.getY()));
+            if (!compareColors(Color.decode((pixel.getAlpha() == 1.0 ? pixel.getImageColor() : pixel.getPlaceColor())), placeColor)) {
+                PlaceData.fixingQ.add(pixel);
+            }
+        }
     }
 
-    private boolean compareColors (Color img, Color place) {
-        return img.getRed() == place.getRed() &&
-                img.getGreen() == place.getGreen() &&
-                img.getBlue() == place.getBlue();
+    private static boolean compareColors (Color img, Color place) {
+        return img.getRed() == place.getRed() && img.getGreen() == place.getGreen() && img.getBlue() == place.getBlue();
     }
 }
