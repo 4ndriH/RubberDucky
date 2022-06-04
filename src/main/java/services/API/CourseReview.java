2abcfe6d2cfa2549aec06d8039fac374d54475f3
4ndriH <handri@bluewin.ch>
2022-06-04 14:58:00 +0200

    api test

---
+++ src/main/java/services/API/CourseReview.java
@@ -0,0 +1,16 @@
+package services.API;
+
+import static spark.Spark.*;
+
+public class CourseReview {
+    static String keyStoreLocation = "-";
+    static String keyStorePassword = "abcd1234";
+
+    public static void api() {
+        //secure(keyStoreLocation, keyStorePassword, null, null);
+        port(5678);
+
+        get("/hello", (request, response) -> "Hello World!");
+    }
+
+}
