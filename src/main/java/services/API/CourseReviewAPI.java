bdb1619e3497664a3b3133ae61cc544359b2d743
4ndriH <handri@bluewin.ch>
2022-06-05 17:30:39 +0200

    api should now respond

--- src/main/java/services/API/CourseReviewAPI.java
+++ src/main/java/services/API/CourseReviewAPI.java
@@ -6,8 +6,8 @@ import services.database.DBHandlerConfig;
 public class CourseReviewAPI {
 
     public static void start() {
-        Javalin app = Javalin.create().start(DBHandlerConfig.getConfig().get("APIAddress"), Integer.parseInt(DBHandlerConfig.getConfig().get("APIPort")));
-        app.get("/test", ctx -> ctx.result("Hello World"));
+        Javalin app = Javalin.create().start(Integer.parseInt(DBHandlerConfig.getConfig().get("APIPort")));
+        app.get("/test", ctx -> ctx.result("Hello World\n"));
     }
 
 }
