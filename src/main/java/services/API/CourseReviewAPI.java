908fc9523aa6dd388cbc2f9be7c1ee1936c6c04e
4ndriH <handri@bluewin.ch>
2022-06-12 11:42:08 +0200

    next ssl api attempt

--- src/main/java/services/API/CourseReviewAPI.java
+++ src/main/java/services/API/CourseReviewAPI.java
@@ -1,13 +1,38 @@
 package services.API;
 
 import io.javalin.Javalin;
-import services.database.DBHandlerConfig;
+import org.eclipse.jetty.server.Connector;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.ServerConnector;
+import org.eclipse.jetty.util.ssl.SslContextFactory;
 
 public class CourseReviewAPI {
 
     public static void start() {
-        Javalin app = Javalin.create().start(Integer.parseInt(DBHandlerConfig.getConfig().get("APIPort")));
-        app.get("/test", ctx -> ctx.result("Hello World\n"));
+//        Javalin app = Javalin.create().start(Integer.parseInt(DBHandlerConfig.getConfig().get("APIPort")));
+
+//        app.get("/test", ctx -> ctx.result("Hello World\n"));
+
+        Javalin.create(config -> {
+            config.server(() -> {
+                Server server = new Server();
+                ServerConnector sslConnector = new ServerConnector(server, getSslContextFactory());
+                sslConnector.setPort(443);
+                ServerConnector connector = new ServerConnector(server);
+                connector.setPort(80);
+                server.setConnectors(new Connector[]{sslConnector, connector});
+                return server;
+            });
+        }).start().get("/test", ctx -> ctx.result("Hello World"));
+
+
+
     }
 
+    private static SslContextFactory getSslContextFactory() {
+        SslContextFactory sslContextFactory = new SslContextFactory();
+        sslContextFactory.setKeyStorePath(CourseReviewAPI.class.getResource("/usr/games/keystore tests/clientkeystore").toExternalForm());
+        sslContextFactory.setKeyStorePassword("password");
+        return sslContextFactory;
+    }
 }
