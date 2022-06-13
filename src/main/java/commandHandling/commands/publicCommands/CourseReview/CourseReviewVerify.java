c0b5fadc0fa7ea79a77ee5e6065b8195eaec7057
h4ndr <handri@bluewin.ch>
2022-06-13 23:03:37 +0200

    removed another block which would prevent admins from using it

--- src/main/java/commandHandling/commands/publicCommands/CourseReview/CourseReviewVerify.java
+++ src/main/java/commandHandling/commands/publicCommands/CourseReview/CourseReviewVerify.java
@@ -44,9 +44,9 @@ public class CourseReviewVerify implements CommandInterface {
             embed.setDescription(entry.getValue()[0]);
             embed.setFooter(entry.getValue()[1]);
             ctx.getChannel().sendMessageEmbeds(embed.build()).setActionRow(
-                    Button.danger("$cfvReject - " + entry.getKey(), "Reject"),
-                    Button.primary("$cfvQuit - " + entry.getKey(), "Quit"),
-                    Button.success("$cfvAccept - " + entry.getKey(), "Accept")
+                    Button.danger("cfvReject - " + entry.getKey(), "Reject"),
+                    Button.primary("cfvQuit - " + entry.getKey(), "Quit"),
+                    Button.success("cfvAccept - " + entry.getKey(), "Accept")
             ).queue();
         } else {
             EmbedHelper.sendEmbed(ctx, EmbedHelper.embedBuilder("Nothing to review"), 32);
