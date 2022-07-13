000254c684fbe0b3aedd5e3c120d4710fb8a1b4d
4ndriH <handri@bluewin.ch>
2022-07-13 21:46:02 +0200

    removed logging

--- src/main/java/services/listener/BGListener.java
+++ src/main/java/services/listener/BGListener.java
@@ -21,12 +21,10 @@ public class BGListener extends ListenerAdapter {
 
         if (event.getAuthor().getId().equals("778731540359675904") && message.getContentRaw().contains("button")) {
             int buttonScore = Integer.parseInt(message.getActionRows().get(0).getButtons().get(0).getLabel());
-            LOGGER.info("Button score updated: " + buttonScore);
             int myCurrentScore = Integer.parseInt(getConfig().get("ButtonScore"));
 
             if (buttonScore > myCurrentScore) {
                 if (--nextNotification <= 0) {
-                    LOGGER.error("New notification sent " + "[\" + myCurrentScore + \" -> \" + buttonScore + \"]");
                     event.getJDA().openPrivateChannelById("155419933998579713").complete().sendMessage(
                             "Button is ready [" + myCurrentScore + " -> " + buttonScore + "]\n" +
                                 "https://discord.com/channels/747752542741725244/" + event.getChannel().getId() +
@@ -44,11 +42,9 @@ public class BGListener extends ListenerAdapter {
 
             if (messageContent.contains("155419933998579713")) {
                 updateConfig("Button Score", messageContent.split(" ")[3]);
-                LOGGER.error("I claimed points and it got updated in the db: " + getConfig().get("ButtonScore"));
             }
 
             if (messageContent.contains("has claimed")) {
-                LOGGER.error("someone else has claimed the button");
                 nextNotification = 0;
             }
         }
