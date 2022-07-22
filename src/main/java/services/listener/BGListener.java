20de9e4c25fc04f4b7b600f650bb07f72b89e334
h4ndr <handri@bluewin.ch>
2022-07-22 11:38:58 +0200

    added logging

--- src/main/java/services/listener/BGListener.java
+++ src/main/java/services/listener/BGListener.java
@@ -28,7 +28,8 @@ public class BGListener extends ListenerAdapter {
                     event.getJDA().openPrivateChannelById("155419933998579713").complete().sendMessage(
                             "Button is ready [" + myCurrentScore + " -> " + buttonScore + "]\n" +
                                 "https://discord.com/channels/747752542741725244/" + event.getChannel().getId() +
-                                "/" + event.getMessage().getId()).queue();
+                                "/" + event.getMessage().getId()
+                    ).queue();
                     nextNotification = 10;
                 }
             }
