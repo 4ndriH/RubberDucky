a9f23f08bde798dc8f93e15a0e047189f25b6a59
4ndriH <handri@bluewin.ch>
2022-10-02 22:07:23 +0200

    more logging

--- src/main/java/services/place/Verifier.java
+++ src/main/java/services/place/Verifier.java
@@ -16,11 +16,15 @@ public class Verifier {
         LOGGER.warn("starting verification");
 
         for (int i = 0; i < PlaceData.drawnPixels; i++) {
+            LOGGER.info("Starting verification of Pixel " + i);
             Pixel pixel = PlaceData.pixels.get(i);
             Color placeColor = new Color(place.getRGB(pixel.getX(), pixel.getY()));
+            LOGGER.info("Pre check of pixel " + i);
             if (!compareColors(Color.decode(pixel.getPlaceColor()), placeColor)) {
                 PlaceData.fixingQ.add(pixel);
+                LOGGER.info("Pixel " + i + " needs to be fixed");
             }
+            LOGGER.info("Post check of pixel " + i);
         }
 
         LOGGER.warn("force reloading image");
