a433668219ad5d61c9bb52b6f5f4746e435e3f79
4ndriH <handri@bluewin.ch>
2022-10-14 18:20:43 +0200

    test with channel ID instead of name

--- src/main/java/services/listeners/ForumListener.java
+++ src/main/java/services/listeners/ForumListener.java
@@ -1,8 +1,6 @@
 package services.listeners;
 
-import net.dv8tion.jda.api.entities.MessageType;
 import net.dv8tion.jda.api.entities.emoji.Emoji;
-import net.dv8tion.jda.api.events.channel.ChannelCreateEvent;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import org.jetbrains.annotations.NotNull;
@@ -15,15 +13,10 @@ public class ForumListener extends ListenerAdapter {
     @Override
     public void onMessageReceived(@NotNull MessageReceivedEvent event) {
         if (!event.getAuthor().isBot() && event.getChannelType().isThread()) {
-            LOGGER.info("thread channel | "+ event.getChannel().asThreadChannel().getParentChannel().getName());
-
-            if (event.getChannel().asThreadChannel().getParentChannel().getName().equalsIgnoreCase("bots-testing")) {
-                LOGGER.info("success");
+            if (event.getChannel().asThreadChannel().getParentChannel().getId().equals("1019663716804997192")) {
+                LOGGER.warn("works with id too");
                 if (System.currentTimeMillis() - event.getChannel().getTimeCreated().toInstant().toEpochMilli() < 1000) {
                     event.getMessage().addReaction(Emoji.fromFormatted("bunnyvibes:989952440126296116")).queue();
-                    LOGGER.info("bunny vibes added");
-                } else {
-                    LOGGER.info("older than 1 second");
                 }
             }
         }
