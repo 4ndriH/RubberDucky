461c71c7b4aeaab510476ccc5610ea08d840b7e3
huandri <huandri@student.ethz.ch>
2022-12-13 16:11:30 +0100

    retry if the image contains transparent pixels

--- src/main/java/services/place/PlaceWebSocket.java
+++ src/main/java/services/place/PlaceWebSocket.java
@@ -3,9 +3,10 @@ package services.place;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import javax.net.ssl.SSLHandshakeException;
+import javax.imageio.ImageIO;
 import java.awt.*;
 import java.awt.image.BufferedImage;
+import java.io.*;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.WebSocket;
@@ -18,77 +19,94 @@ public class PlaceWebSocket {
     private static final Logger LOGGER = LoggerFactory.getLogger(PlaceWebSocket.class);
 
     public static BufferedImage getImage (boolean colored) {
+        BufferedImage img;
         ByteBuffer buffer;
+        int retryCnt = 0;
 
         do {
-            CountDownLatch latch = new CountDownLatch(1);
-            WebSocketClient wsc = new WebSocketClient(latch);
-            WebSocket ws;
+            img = new BufferedImage(1000, 1000, BufferedImage.TYPE_INT_ARGB);
 
-            try {
-                ws = HttpClient
-                    .newHttpClient()
-                    .newWebSocketBuilder()
-                    .buildAsync(URI.create("wss://ws.battlerush.dev/"), wsc)
-                    .join();
-            } catch(Exception e) {
-                if(e.getMessage().contains("javax.net.ssl.SSLHandshakeException")) {
+            do {
+                CountDownLatch latch = new CountDownLatch(1);
+                WebSocketClient wsc = new WebSocketClient(latch);
+                WebSocket ws;
+
+                try {
+                    ws = HttpClient
+                            .newHttpClient()
+                            .newWebSocketBuilder()
+                            .buildAsync(URI.create("wss://ws.battlerush.dev/"), wsc)
+                            .join();
+                    if (retryCnt++ >= 5) {
+                        throw new IOException("Stopped after 5 retries");
+                    }
+                } catch (IOException e) {
+                  LOGGER.error("5 RETRIES ARE ENOUGH", e);
+                  return img;
+                } catch(Exception e) {
                     LOGGER.error("Websocket Problem", e);
-                    return new BufferedImage(1000, 1000, BufferedImage.TYPE_INT_ARGB);
-                } else {
-                    LOGGER.error("Websocket Problem", e);
-                    return new BufferedImage(1000, 1000, BufferedImage.TYPE_INT_ARGB);
+                    return img;
+                }
+
+                WebSocketClient.buffer = ByteBuffer.allocate(0);
+                ws.sendText(""+(char)1, true);
+
+                try {
+                    latch.await();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+                buffer = WebSocketClient.buffer;
+                ws.abort();
+            } while (buffer.remaining() <= 3000000);
+
+            int x = 0, y = 0;
+
+            buffer.position(0);
+            buffer.get();
+
+            // write the received data to a buffered image, either colored or grey scaled
+            if (buffer.remaining() >= 3000000) {
+                while (buffer.hasRemaining() && y < 1000) {
+                    int r = 255&buffer.get(), g = 255&buffer.get(), b = 255&buffer.get();
+                    Color color;
+
+                    if (colored) {
+                        color = new Color(r, g, b);
+                    } else {
+                        int rgb = (int)(r * 0.299) + (int)(g * 0.587) + (int)(b * 0.114);
+                        color = new Color(rgb, rgb, rgb);
+                    }
+
+                    img.setRGB(x++, y, color.getRGB());
+                    if (x == 1000) {
+                        x = 0;
+                        y++;
+                    }
                 }
             }
 
-            WebSocketClient.buffer = ByteBuffer.allocate(0);
-            ws.sendText(""+(char)1, true);
-
-            try {
-                latch.await();
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-
-            buffer = WebSocketClient.buffer;
-            ws.abort();
-        } while (buffer.remaining() <= 3000000);
-
-        BufferedImage img = new BufferedImage(1000, 1000, BufferedImage.TYPE_INT_ARGB);
-        int x = 0, y = 0;
-
-        buffer.position(0);
-        buffer.get();
-
-        // write the received data to a buffered image, either colored or grey scaled
-        if (buffer.remaining() >= 3000000) {
-            while (buffer.hasRemaining() && y < 1000) {
-                int r = 255&buffer.get(), g = 255&buffer.get(), b = 255&buffer.get();
-                Color color;
-
-                if (colored) {
-                    color = new Color(r, g, b);
-                } else {
-                    int rgb = (int)(r * 0.299) + (int)(g * 0.587) + (int)(b * 0.114);
-                    color = new Color(rgb, rgb, rgb);
-                }
-
-                img.setRGB(x++, y, color.getRGB());
-                if (x == 1000) {
-                    x = 0;
-                    y++;
+            // if the image is not complete, try again
+            for (int i = 0; i < 1000; i++) {
+                if (new Color(img.getRGB(i, i), true).getAlpha() == 0) {
+                    img = null;
+                    break;
                 }
             }
-        }
-
-        // Sleep so the buffered image is not just black before it gets returned
-        try {
-            Thread.sleep(128);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+        } while (img == null);
 
         return img;
+    }
+
+    private static InputStream convert(BufferedImage img) {
+        ByteArrayOutputStream os = new ByteArrayOutputStream();
+        try {
+            ImageIO.write(img, "png", os);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return new ByteArrayInputStream(os.toByteArray());
     }
 
     private static class WebSocketClient implements WebSocket.Listener {
