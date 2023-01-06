d49db90179406a6124268d7cf4a6864ad9c398de
huandri <huandri@student.ethz.ch>
2023-01-06 14:13:25 +0100

    wip

---
+++ src/main/java/assets/CHANNELS.java
@@ -0,0 +1,15 @@
+package assets;
+
+public class CHANNELS {
+    // DINFK channels
+    public static final String ethPlaceBots = "819966095070330950";
+    public static final String ethPlaceBots2 = "955751651942211604";
+    public static final String spam = "768600365602963496";
+
+    // RubberDuckyDev channels
+    public static final String randomShit = "1020951518582673478";
+    public static final String hq = "991686525651800175";
+    public static final String botExceptionLog = "841393155478650920";
+    public static final String devBotExceptions = "865693419376738315";
+    public static final String placeLog = "969901898389925959";
+}
---
+++ src/main/java/assets/SERVERS.java
@@ -0,0 +1,6 @@
+package assets;
+
+public class SERVERS {
+    public static final String RubberDuckyDev = "817850050013036605";
+    public static final String DINFK = "747752542741725244";
+}
