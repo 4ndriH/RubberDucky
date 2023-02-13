ccbfd3e6c50325a7bd38f3ceea2f134c80db2ebd
4ndriH <45722342+4ndriH@users.noreply.github.com>
2023-02-13 15:42:13 +0100

    Lower case method names

--- src/main/java/services/discordHelpers/Compare.java
+++ src/main/java/services/discordHelpers/Compare.java
@@ -4,15 +4,15 @@ import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.entities.channel.Channel;
 
 public class Compare {
-    public static boolean Channels(Channel c1, Channel c2) {
+    public static boolean channels(Channel c1, Channel c2) {
         return c1.getId().equals(c2.getId());
     }
 
-    public static boolean Channels(Channel c, String id) {
+    public static boolean channels(Channel c, String id) {
         return c.getId().equals(id);
     }
 
-    public static boolean MessageContent(Message msg, String content, boolean contains, boolean ignoreCase) {
+    public static boolean messageContent(Message msg, String content, boolean contains, boolean ignoreCase) {
         if (!contains) {
             if (ignoreCase) {
                 return msg.getContentRaw().equalsIgnoreCase(content);
@@ -28,7 +28,7 @@ public class Compare {
         }
     }
 
-    public static boolean MessageContent(Message msg, String content) {
-        return MessageContent(msg, content, false, false);
+    public static boolean messageContent(Message msg, String content) {
+        return messageContent(msg, content, false, false);
     }
 }
