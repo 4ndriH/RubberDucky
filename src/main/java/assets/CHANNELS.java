9c8eb2d14c31c51ba53a215eb6d0bf49fdafa58a
huandri <huandri@student.ethz.ch>
2023-02-13 15:29:29 +0100

    prevent null pointers on initialization (need to look into NPs during use)

--- src/main/java/assets/CHANNELS.java
+++ src/main/java/assets/CHANNELS.java
@@ -18,15 +18,19 @@ public class CHANNELS {
     public static TextChannel placeLog;
 
     public CHANNELS() {
-        ethPlaceBots = SERVERS.DINFK.getTextChannelById("819966095070330950");
-        ethPlaceBots2 = SERVERS.DINFK.getTextChannelById("955751651942211604");
-        spam = SERVERS.DINFK.getTextChannelById("768600365602963496");
-        toInfinityAndBeyond = SERVERS.DINFK.getThreadChannelById("996746797236105236");
+        if (SERVERS.DINFK != null) {
+            ethPlaceBots = SERVERS.DINFK.getTextChannelById("819966095070330950");
+            ethPlaceBots2 = SERVERS.DINFK.getTextChannelById("955751651942211604");
+            spam = SERVERS.DINFK.getTextChannelById("768600365602963496");
+            toInfinityAndBeyond = SERVERS.DINFK.getThreadChannelById("996746797236105236");
+        }
 
-        randomShit = SERVERS.RubberDuckyDev.getTextChannelById("1020951518582673478");
-        hq = SERVERS.RubberDuckyDev.getTextChannelById("991686525651800175");
-        botExceptionLog = SERVERS.RubberDuckyDev.getTextChannelById("841393155478650920");
-        devBotExceptions = SERVERS.RubberDuckyDev.getTextChannelById("865693419376738315");
-        placeLog = SERVERS.RubberDuckyDev.getTextChannelById("969901898389925959");
+        if (SERVERS.RubberDuckyDev != null) {
+            randomShit = SERVERS.RubberDuckyDev.getTextChannelById("1020951518582673478");
+            hq = SERVERS.RubberDuckyDev.getTextChannelById("991686525651800175");
+            botExceptionLog = SERVERS.RubberDuckyDev.getTextChannelById("841393155478650920");
+            devBotExceptions = SERVERS.RubberDuckyDev.getTextChannelById("865693419376738315");
+            placeLog = SERVERS.RubberDuckyDev.getTextChannelById("969901898389925959");
+        }
     }
 }
