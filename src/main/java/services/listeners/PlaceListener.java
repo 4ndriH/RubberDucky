6191253b3bf3b512c43311352e252d793bc8fde8
Andri Hunkeler <handri@bluewin.ch>
2024-01-30 21:20:52 +0100

    listening for new chunks

--- src/main/java/services/listeners/PlaceListener.java
+++ src/main/java/services/listeners/PlaceListener.java
@@ -3,14 +3,33 @@ package services.listeners;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import org.jetbrains.annotations.NotNull;
-import assets.objects.PlaceData;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class PlaceListener extends ListenerAdapter {
+    private final Logger LOGGER = LoggerFactory.getLogger(PlaceListener.class);
+    private static String timelapseMatch = "Saved Chunk_";
+
     @Override
     public void onMessageReceived(@NotNull MessageReceivedEvent event) {
-        if (event.getAuthor().isBot() && event.getChannel().getId().matches("819966095070330950|955751651942211604")) {
-            if (event.getMessage().getContentRaw().contains("817846061347242026")) {
-                PlaceData.addPixelRequest(event.getAuthor().getId());
+        if (event.getAuthor().isBot()) {
+//            if (event.getChannel().getId().equals("819966095070330950")) {
+//                if (event.getMessage().getContentRaw().contains("817846061347242026")) {
+//                    PlaceData.addPixelRequest(event.getAuthor().getId());
+//                }
+//            }
+
+            if (event.getAuthor().getId().equals("774276700557148170") && event.getMessage().getContentRaw().startsWith(timelapseMatch)) {
+                String nr = event.getMessage().getContentRaw().split("_")[1].split("\\.")[0];
+                int chunk = 0;
+                try {
+                    chunk = Integer.parseInt(nr);
+                } catch (NumberFormatException e) {
+                    LOGGER.warn("Could not parse chunk number", e);
+                }
+
+                LOGGER.warn("got chunk number " + chunk, new InterruptedException());
+                LOGGER.info("got chunk number " + chunk, new InterruptedException());
             }
         }
     }
