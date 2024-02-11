d9fb943e29c9bdae669f9de0165be0cf0921fcb8
Andri Hunkeler <handri@bluewin.ch>
2024-02-11 12:36:03 +0100

    added more snooping stuff

--- src/main/java/services/listeners/PlaceListener.java
+++ src/main/java/services/listeners/PlaceListener.java
@@ -2,14 +2,18 @@ package services.listeners;
 
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import org.apache.commons.collections4.queue.CircularFifoQueue;
 import org.jetbrains.annotations.NotNull;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import services.place.TimelapseHelper;
 
+import java.util.Queue;
+
 public class PlaceListener extends ListenerAdapter {
     private final Logger LOGGER = LoggerFactory.getLogger(PlaceListener.class);
-    private static String timelapseMatch = "Saved Chunk_";
+    private static final String timelapseMatch = "Saved Chunk_";
+    private static Queue<String> logBuffer = new CircularFifoQueue<>(8);
 
     @Override
     public void onMessageReceived(@NotNull MessageReceivedEvent event) {
@@ -37,7 +41,15 @@ public class PlaceListener extends ListenerAdapter {
 
             if (event.getAuthor().getId().equals("735170037282898061") && event.getMessage().getContentRaw().startsWith(".place setpixel 418 718")) {
                 LOGGER.info("rafeal restarted from the top");
+
+                StringBuilder sb = new StringBuilder();
+                for (String s : logBuffer) {
+                    sb.append(s).append("\n");
+                }
+                event.getJDA().getGuildById("817850050013036605").getTextChannelById("841393155478650920").sendMessage(sb).queue();
             }
+        } else if (event.getAuthor().getId().equals("378255099748679681")) {
+            logBuffer.add(event.getMessage().getContentRaw());
         }
     }
 }
