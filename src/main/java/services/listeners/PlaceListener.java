6879c7d1e13219596b67bc0d24a6fe4404500979
Andri Hunkeler <handri@bluewin.ch>
2024-02-12 20:05:55 +0100

    removed snoop

--- src/main/java/services/listeners/PlaceListener.java
+++ src/main/java/services/listeners/PlaceListener.java
@@ -13,7 +13,6 @@ import java.util.Queue;
 public class PlaceListener extends ListenerAdapter {
     private final Logger LOGGER = LoggerFactory.getLogger(PlaceListener.class);
     private static final String timelapseMatch = "Saved Chunk_";
-    private static Queue<String> logBuffer = new CircularFifoQueue<>(8);
 
     @Override
     public void onMessageReceived(@NotNull MessageReceivedEvent event) {
@@ -38,18 +37,6 @@ public class PlaceListener extends ListenerAdapter {
                 int finalChunk = chunk;
                 (new Thread(() -> TimelapseHelper.generate(finalChunk, event))).start();
             }
-
-            if (event.getAuthor().getId().equals("735170037282898061") && event.getMessage().getContentRaw().startsWith(".place setpixel 418 718")) {
-                LOGGER.info("rafeal restarted from the top");
-
-                StringBuilder sb = new StringBuilder();
-                for (String s : logBuffer) {
-                    sb.append(s).append("\n");
-                }
-                event.getJDA().getGuildById("817850050013036605").getTextChannelById("841393155478650920").sendMessage(sb).queue();
-            }
-        } else if (event.getAuthor().getId().equals("378255099748679681")) {
-            logBuffer.add(event.getMessage().getContentRaw());
         }
     }
 }
