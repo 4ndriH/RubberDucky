99fe1f4b4763dff9604f3c75cbf31f32829149bd
Andri Hunkeler <handri@bluewin.ch>
2024-02-16 19:57:30 +0100

    adding a method I removed by accident

--- src/main/java/commandhandling/commands/place/PlaceVerify.java
+++ src/main/java/commandhandling/commands/place/PlaceVerify.java
@@ -13,7 +13,6 @@ import assets.objects.PlaceData;
 import javax.imageio.ImageIO;
 import java.io.IOException;
 import java.net.URL;
-import java.util.List;
 
 public class PlaceVerify implements CommandInterface {
     private final Logger LOGGER = LoggerFactory.getLogger(PlaceVerify.class);
@@ -34,6 +33,11 @@ public class PlaceVerify implements CommandInterface {
 
             PlaceData.verify = true;
         }
+    }
+
+    @Override
+    public String getName() {
+        return "PlaceVerify";
     }
 
     @Override
