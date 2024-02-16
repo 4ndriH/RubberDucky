a45619075eeaf560bae645ac19aab0a1fabe8254
Andri Hunkeler <handri@bluewin.ch>
2024-02-16 21:04:07 +0100

    logback now logs errors/warns even when there are no exceptions provided (#37)

--- src/main/java/services/logging/DiscordAppender.java
+++ src/main/java/services/logging/DiscordAppender.java
@@ -10,7 +10,6 @@ import java.awt.*;
 
 public class DiscordAppender extends AppenderBase<ILoggingEvent> {
     private static JDA jda;
-    private static boolean startUpBlock = true;
 
     public static void setJDA(JDA jda) {
         DiscordAppender.jda = jda;
@@ -18,36 +17,29 @@ public class DiscordAppender extends AppenderBase<ILoggingEvent> {
 
     @Override
     protected void append(ILoggingEvent eventObject) {
-        StringBuilder sb = new StringBuilder().append("```");
         EmbedBuilder embed = new EmbedBuilder();
 
-        for (StackTraceElement s : eventObject.getCallerData()) {
-            sb.append(s).append("\n");
-        }
+        Color embedColor = switch (eventObject.getLevel().toString()) {
+            case "INFO" -> new Color(0x42a2fc);
+            case "WARN" -> new Color(0xff9100);
+            case "ERROR" -> new Color(0xff0000);
+            default -> new Color(0x000000);
+        };
 
-        sb.append("```");
-
-        embed.setColor(eventObject.getLevel().toString().equals("WARN") ? new Color(0xff9100) : new Color(0xff0000));
+        embed.setColor(embedColor);
         embed.setTitle(eventObject.getLevel() + ": " + eventObject.getMessage());
 
-        if (sb.length() > 4096) {
-            embed.setDescription(sb.substring(0, 4096));
-        } else {
-            embed.setDescription(sb);
-        }
-
-        embed.setFooter(eventObject.getThrowableProxy().getClassName());
-
-        jda.getGuildById("817850050013036605").getTextChannelById(Config.logChannelID)
-                .sendMessageEmbeds(embed.build()).queue();
-
-
-        if (eventObject.getMessage().equals("Websocket Dead")) {
-            if (!startUpBlock) {
-                jda.openPrivateChannelById("153929916977643521").complete().sendMessage("Hi\nI tried accessing the websocket and as it turns out, it is (still) dead.\nCould you please fix it?").queue();
-            } else {
-                startUpBlock = false;
+        if (eventObject.getThrowableProxy() != null) {
+            StringBuilder sb = new StringBuilder().append("```");
+            for (StackTraceElement s : eventObject.getCallerData()) {
+                sb.append(s).append("\n");
             }
+            sb.append("```");
+
+            embed.setDescription(sb.length() > 4096 ? sb.substring(0, 4096) : sb.toString());
+            embed.setFooter(eventObject.getThrowableProxy().getClassName());
         }
+
+        jda.getGuildById("817850050013036605").getTextChannelById(Config.logChannelID).sendMessageEmbeds(embed.build()).queue();
     }
 }
