79783c0b871b71ab9a94cdbb68077304c4520b25
Andri Hunkeler <handri@bluewin.ch>
2025-06-29 11:23:29 +0200

    remove coursereview

--- src/main/java/services/CommandManager.java
+++ src/main/java/services/CommandManager.java
@@ -6,7 +6,6 @@ import commandhandling.CommandInterface;
 import commandhandling.commands.admin.LockDown;
 import commandhandling.commands.admin.Nuke;
 import commandhandling.commands.admin.Watch;
-import commandhandling.commands.coursereview.CourseReviewVerify;
 import commandhandling.commands.mod.BlackList;
 import commandhandling.commands.mod.Channel;
 import commandhandling.commands.owner.*;
@@ -81,9 +80,6 @@ public class CommandManager {
         addCommand(new PlaceView());
         addCommand(new PlaceViewQueue());
         addCommand(new PlaceInfinite());
-
-        // course review
-        addCommand(new CourseReviewVerify());
 
         LOGGER.info(commands.size() + " commands loaded");
     }
