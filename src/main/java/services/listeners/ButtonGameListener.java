70b200a20324a13224c8d18f5ab336ec4f84da1a
Andri Hunkeler <handri@bluewin.ch>
2025-06-29 11:19:33 +0200

    buttonscore saved to correct row

--- src/main/java/services/listeners/ButtonGameListener.java
+++ src/main/java/services/listeners/ButtonGameListener.java
@@ -2,8 +2,6 @@ package services.listeners;
 
 import assets.Config;
 import net.dv8tion.jda.api.entities.Message;
-import net.dv8tion.jda.api.entities.MessageEmbed;
-import net.dv8tion.jda.api.entities.channel.concrete.TextChannel;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.events.message.MessageUpdateEvent;
 import net.dv8tion.jda.api.events.session.ReadyEvent;
@@ -11,14 +9,6 @@ import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import org.jetbrains.annotations.NotNull;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import services.discordhelpers.EmbedHelper;
-
-import java.time.LocalDate;
-import java.util.Objects;
-import java.util.List;
-
-import static services.database.DBHandlerCourseReviewVerify.unverifiedReviewsExist;
-
 
 public class ButtonGameListener extends ListenerAdapter {
     private static final Logger LOGGER = LoggerFactory.getLogger(ButtonGameListener.class);
@@ -28,29 +18,6 @@ public class ButtonGameListener extends ListenerAdapter {
     public void onReady(@NotNull ReadyEvent event) {
         if (!event.getJDA().getSelfUser().getId().equals("817846061347242026")) {
             event.getJDA().removeEventListener(this);
-        }
-
-        TextChannel channel = Objects.requireNonNull(event.getJDA().getGuildById("817850050013036605")).getTextChannelById("988081117015973918");
-
-        if (channel == null) {
-            LOGGER.error("Could not find the channel for unverified reviews.");
-            return;
-        } else if (event.getJDA().getSelfUser().getId().equals("865483954075992086")) {
-            return;
-        }
-
-        for (Message message : channel.getIterableHistory()) {
-            List<MessageEmbed> embeds = message.getEmbeds();
-
-            if (embeds.isEmpty()) {
-                continue;
-            }
-
-            if (Objects.requireNonNull(embeds.get(0).getTitle()).contains("Unverified")) {
-                notificationMessageID = message.getId();
-                LOGGER.info("Found the notification message for unverified reviews.\nMessage ID: {}", notificationMessageID);
-                break;
-            }
         }
     }
 
@@ -71,9 +38,6 @@ public class ButtonGameListener extends ListenerAdapter {
                     nextNotification = 10;
                 }
             }
-
-            // This is temporary
-            dailyCheckForUnreviewedReviews(event);
         }
     }
 
@@ -86,34 +50,11 @@ public class ButtonGameListener extends ListenerAdapter {
                 String score = messageContent.replace("155419933998579713", "").replaceAll("\\D", "");
                 LOGGER.debug("Button Score Updated. New Score: " + score);
                 myCurrentScore = Integer.parseInt(score);
-                Config.updateConfig("ButtonScore", score);
+                Config.updateConfig("buttonScore", score);
             }
 
             if (messageContent.contains("has claimed")) {
                 nextNotification = 0;
-            }
-        }
-    }
-
-    private LocalDate lastCheckedDate = null;
-    public static String notificationMessageID = null;
-
-    private void dailyCheckForUnreviewedReviews(MessageUpdateEvent event) {
-        LocalDate currentDate = LocalDate.now();
-
-        if (lastCheckedDate == null || !lastCheckedDate.equals(currentDate)) {
-            lastCheckedDate = currentDate;
-
-            if (unverifiedReviewsExist()) {
-                Message message = Objects.requireNonNull(Objects.requireNonNull(event.getJDA().getGuildById("817850050013036605")).getTextChannelById("988081117015973918")).sendMessageEmbeds(
-                        EmbedHelper.embedBuilder("Unverified Reviews Exist")
-                                .setDescription("Go verify them!")
-                                .setFooter("Yes, I do it now. Also dont worry about how I do it. I am a bot. I am smart.")
-                                .build()
-                ).complete();
-
-                notificationMessageID = message.getId();
-                LOGGER.info("Notification message for unverified reviews sent.\nMessage ID: {}", notificationMessageID);
             }
         }
     }
